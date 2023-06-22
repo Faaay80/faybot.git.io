eaf09d484b7e3ffd122853693d15ff1476e2f2ae
Faaay80 <137386028+Faaay80@users.noreply.github.com>
2023-06-22 16:46:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+/* clique button pour le chnagement de nombre */
+
+let cookieCount = 0;
+const cookie = document.getElementById('cookie');
+const cookieCountSpan = document.getElementById('cookie-count');
+const clickButton = document.getElementById('click-button');
+
+cookie.addEventListener('click', function () {
+    cookieCount++;
+    cookieCountSpan.textContent = cookieCount;
+});
+
+clickButton.addEventListener('click', function () {
+    cookieCount += 10;
+    cookieCountSpan.textContent = cookieCount;
+});
+
+/* clique button pour le chnagement de nombre */
+
+document.getElementById("click-button").addEventListener("click", function() {
+    var cookie = document.getElementById("cookie");
+    cookie.classList.add("roll-animation");
+    
+    setTimeout(function() {
+        cookie.classList.remove("roll-animation");
+    }, 1000); // Durée de l'animation en millisecondes (ici, 1 seconde)
+});
+
+
+
+
+
+
+
+
+
+
+
+
