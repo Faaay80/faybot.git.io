b589ab9ee71c73a96c373dfbb597c716ce73e154
Faaay80 <137386028+Faaay80@users.noreply.github.com>
2023-06-29 16:42:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,27 +5,37 @@ let cookieCount = 0;
 const cookie = document.getElementById('cookie');
 const cookieCountSpan = document.getElementById('cookie-count');
 const clickButton = document.getElementById('click-button');
+const idleButton = document.getElementById('idle');
 
 cookie.addEventListener('click', function () {
     cookieCount++;
     cookieCountSpan.textContent = cookieCount;
 });
 
-clickButton.addEventListener('click', function () {
-    cookieCount += 10;
-    cookieCountSpan.textContent = cookieCount;
-});
+
 
 /* clique button pour le chnagement de nombre */
 
-document.getElementById("click-button").addEventListener("click", function() {
-    var cookie = document.getElementById("cookie");
-    cookie.classList.add("heartbeat-animation");
+// document.getElementById("click-button").addEventListener("click", function() {
+//     var cookie = document.getElementById("cookie");
+//     cookie.classList.add("heartbeat-animation");
     
-    setTimeout(function() {
-        cookie.classList.remove("heartbeat-animation");
-    }, 1000); // Durée de l'animation en millisecondes (ici, 1 seconde)
+//     setTimeout(function() {
+//         cookie.classList.remove("heartbeat-animation");
+//     }, 1000); // Durée de l'animation en millisecondes (ici, 1 seconde)
+// });
+
+
+
+
+idleButton.addEventListener('click', function () {
+    console.log("sdfghjk")
+    if(cookieCount >= 100) {
+        cookieCount -= 100
+        cookie.src = this.querySelector('img').src
+    }
 });
+
 
 
 
